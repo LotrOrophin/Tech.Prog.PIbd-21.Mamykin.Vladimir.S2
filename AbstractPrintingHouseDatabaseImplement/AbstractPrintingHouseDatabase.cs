26f0ab4c8678cf002609651625f19028919f9722
LotrOrophin <mamykin.vladimir@list.ru>
2020-06-05 16:00:36 +0400

    Lab3

---
+++ AbstractPrintingHouseDatabaseImplement/AbstractPrintingHouseDatabase.cs
@@ -0,0 +1,24 @@
+﻿
+using AbstractPrintingHouseDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace AbstractPrintingHouseDatabaseImplement
+{
+    public class AbstractPrintingHouseDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-2VLTMI2\SQLEXPRESS;Initial Catalog=PrintingHouse1Database;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<OfficeComponent> Components { set; get; }
+        public virtual DbSet<PrintingProduct> Products { set; get; }
+        public virtual DbSet<ProductOfficeComponent> ProductComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
+
+    
---
+++ AbstractPrintingHouseDatabaseImplement/Models/ProductOfficeComponent.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AbstractPrintingHouseDatabaseImplement.Models
+{
+ /// <summary>
+ /// Сколько компонентов, требуется при изготовлении изделия
+ /// </summary>
+ public class ProductOfficeComponent
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ComponentId { get; set; }
+        [Required]
+        public int Count { get; set; }
+        public virtual OfficeComponent Component { get; set; }
+
+        public virtual PrintingProduct PrintingProduct { get; set; }
+
+    }
+}   
