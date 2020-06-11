95c4b7a872606be682e1186a0b1f0917db580ea5
LotrOrophin <mamykin.vladimir@list.ru>
2020-06-11 10:05:58 +0400

    changes

--- AbstractPrintingHouseView/Program.cs
+++ AbstractPrintingHouseView/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AbstractPrintingHouseBusinessLogic.BusinessLogics;
 using AbstractPrintingHouseBusinessLogic.Interfaces;
-using AbstractPrintingHouseDatabaseImplement.Implements;
+using AbstractPrintingHouseFileImplement.Implements;
 using System;
 using System.Windows.Forms;
 using Unity;
