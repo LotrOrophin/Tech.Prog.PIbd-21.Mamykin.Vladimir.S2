4b6aed3b1f70b8cb9477496baa0f943bb17b7d45
LotrOrophin <mamykin.vladimir@list.ru>
2020-07-02 12:37:40 +0400

    mod

---
+++ AbstractPrintingHouseFileImplement/Models/PrintingProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractPrintingHouseFileImplement.Models
+{
+    /// <summary>
+    /// Изделие, изготавливаемое в магазине
+    /// </summary>
+    public class PrintingProduct
+    {
+        public int Id { get; set; }
+        public string PrintProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
